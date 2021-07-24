170d790ef629f2fba0b151e45bd2a574e5862367
grace butler <gmx.butler@gmail.com>
2021-07-23 20:59:08 -0500

    schema.sql creates db and tables

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- deletes the data base if company_db already exists and creates it --
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+-- 'cd'/allows us to use the created db --
+USE company_db;
+
+-- create department table --
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+-- create role table --
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL(45.00),
+  department_id INT
+);
+
+-- create employee table --
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
+  );
