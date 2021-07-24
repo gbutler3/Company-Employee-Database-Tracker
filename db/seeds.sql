4237e4e54fa09ea1d02ab982a7d2152f5146bb07
grace butler <gmx.butler@gmail.com>
2021-07-24 17:13:16 -0500

    added data to seeds.sql file. Tested and runs in mysql powershell

---
+++ db/seeds.sql
@@ -0,0 +1,41 @@
+USE company_db;
+
+INSERT INTO department (name)
+VALUES  ("Management"),
+        ("Reception"),
+        ("Sales"),
+        ("Accounting"),
+        ("Human Resources"),
+        ("Customer Service"),
+        ("Warehouse");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Regional Manager", 150000, 1),
+        ("Receptionist", 85000, 2),
+        ("Sales Lead",130000, 3),
+        ("Sales Representative",110000, 4),
+        ("Accountant", 85000, 4),
+        ("HR Specialist", 60000, 5),
+        ("CX Representative", 65000, 6),
+        ("Warehouse Lead ", 90000, 7),
+        ("Warehouse Associate", 80000, 7);
+
+Insert INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Michael", "Scott", 1, null), -- management --
+        ("Pam", "Beesly", 2 , 1), -- reception --
+        ("Dwight", "Schrute", 3 , 1), -- sales --
+        ("Jim", "Halpert", 4, 3), -- sales --
+        ("Phyllis", "Vance", 4, 3), -- sales --
+        ("Stanley", "Hudson", 4, 3), -- sales -- 
+        ("Angela", "Martin", 5, 1), -- accounting --   
+        ("Kevin", "Malone", 5, 1), -- Accounting --
+        ("Oscar", "Martinez", 5, 1), -- Accounting --
+        ("Toby", "Flenderson", 6, 1), -- HR --
+        ("Creed", "Bratton", 7, 1), -- customer service --
+        ("Meredith", "Palmer", 7, 1), -- Customer Service --
+        ("Kelly", "Kapoor", 7,1), -- customer service --
+        ("Darryl", "Philbin", 8, null), -- Warehouse --
+        ("Roy", "Anderson", 9, 14), -- warehouse --
+        ("Val", "Johnson", 9, 14); -- warehouse --
+
+
